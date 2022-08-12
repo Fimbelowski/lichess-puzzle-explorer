bf3260069087212688a6c0eed4e0d5019133020c
Christopher <ChrFimbel@gmail.com>
2022-08-11 20:45:58 -0600

    Use router.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
+import {
+  createRouter,
+  createWebHistory,
+} from 'vue-router';
 import { createApp } from 'vue';
 
 import '@/style.css';
 import App from '@/App.vue';
+import routes from '@/router/routes';
 
-createApp(App).mount('#app');
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
