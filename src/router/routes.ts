65aa09d55d857f6e0c8ab764f040587a1fea5272
Christopher <ChrFimbel@gmail.com>
2022-08-13 01:29:31 -0600

    Update routes.

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,14 +1,23 @@
 import { RouteRecordRaw } from 'vue-router';
 
 import HelloWorld from '@/components/HelloWorld.vue';
+import WarningView from '@/views/WarningView.vue';
 
 const routes: RouteRecordRaw[] = [
   {
+    beforeEnter: (to, from, next) => {
+      console.log('No "puzzles.csv" found. Redirecting...');
+      next('/warning');
+    },
     component: HelloWorld,
     props: {
       msg: 'Vite + Vue',
     },
     path: '/',
+  },
+  {
+    component: WarningView,
+    path: '/warning',
   },
 ];
 
