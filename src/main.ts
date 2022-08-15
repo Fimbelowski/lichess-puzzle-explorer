48e905bfc5daf5758fc0358c9c85a101de5afb64
Christopher <ChrFimbel@gmail.com>
2022-08-14 18:47:10 -0600

    Create main Pinia instance.

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,20 @@ import {
   createWebHistory,
 } from 'vue-router';
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
 import '@/style.css';
 import App from '@/App.vue';
 import routes from '@/router/routes';
 
+const app = createApp(App);
 const router = createRouter({
   history: createWebHistory(),
   routes,
 });
+const store = createPinia();
 
-const app = createApp(App);
 app.use(router);
+app.use(store);
+
 app.mount('#app');
