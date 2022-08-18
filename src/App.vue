f9f7d458739d9158610f978d4a772c336f009b59
Christopher <ChrFimbel@gmail.com>
2022-08-17 22:26:27 -0600

    Move ability to open database connection into usePuzzleDatabase.

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,21 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import { useRouter } from 'vue-router';
-
 import usePuzzleDatabase from '@/composables/usePuzzleDatabase';
 
-const router = useRouter();
 const puzzleDatabase = usePuzzleDatabase();
-
-const databaseExists = ref(false);
-const failedToOpenDatabase = ref(false);
-const databaseErrorMessage = ref('');
-
-const openDatabaseRequest = puzzleDatabase.open();
-openDatabaseRequest.onsuccess = () => {
-  databaseExists.value = true;
-
-  const { result } = openDatabaseRequest;
-
-  if (result.objectStoreNames.length === 0) {
-    router.replace('/warning');
-  }
-};
-openDatabaseRequest.onerror = (event) => {
-  const target = event.target as IDBOpenDBRequest;
-  databaseErrorMessage.value = `Database error: ${target.error}`;
-};
 </script>
 
 <template>
   <router-view
-    v-if="databaseExists"
+    v-if="puzzleDatabase.databaseExists"
   />
   <p
-    v-else-if="failedToOpenDatabase"
+    v-else-if="puzzleDatabase.isError"
   >
-    {{ databaseErrorMessage }}
+    {{ puzzleDatabase.errorMessage }}
   </p>
   <p
     v-else
   >
-    Checking database...
+    Loading database...
   </p>
 </template>
--- src/composables/usePuzzleDatabase.ts
+++ src/composables/usePuzzleDatabase.ts
@@ -1,12 +1,43 @@
+import {
+  computed,
+  ref,
+} from 'vue';
+
 const DATABASE_NAME = 'puzzles';
 const DATABASE_VERSION = 1;
+// const OBJECT_STORE_NAME = 'puzzles';
 
-export default function useIndexedDb() {
-  function open() {
-    return window.indexedDB.open(DATABASE_NAME, DATABASE_VERSION);
-  }
+export default function usePuzzleDatabase() {
+  const database = ref<IDBDatabase>();
+  const isError = ref(false);
+  const errorMessage = ref('');
+
+  const databaseExists = computed(() => database.value !== undefined);
+
+  const openDatabaseRequest = window.indexedDB.open(DATABASE_NAME, DATABASE_VERSION);
+  openDatabaseRequest.onerror = (event) => {
+    isError.value = true;
+
+    const target = event.target as IDBOpenDBRequest;
+    errorMessage.value = `Database Error: ${target.error}`;
+    console.error(errorMessage.value);
+  };
+  openDatabaseRequest.onsuccess = (event) => {
+    // A connection to the database is opened successfully. If database.value is undefined here, the
+    // database already exists.
+    const target = event.target as IDBOpenDBRequest;
+    database.value = target.result;
+  };
+  openDatabaseRequest.onupgradeneeded = (event) => {
+    // Database is created for the first time or is opened with a version number higher than the
+    // existing one.
+    const target = event.target as IDBOpenDBRequest;
+    database.value = target.result;
+  };
 
   return {
-    open,
+    databaseExists,
+    isError,
+    errorMessage,
   };
 }
