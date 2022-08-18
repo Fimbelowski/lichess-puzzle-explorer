d0964c9974917af21ecf1da816ca65174a2a6fc1
Christopher <ChrFimbel@gmail.com>
2022-08-18 15:11:59 -0600

    Use Pinia.

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,21 @@ import {
   createWebHistory,
 } from 'vue-router';
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
 import '@/style.css';
 import App from '@/App.vue';
 import routes from '@/router/routes';
 
+const app = createApp(App);
+
+const pinia = createPinia();
+app.use(pinia);
+
 const router = createRouter({
   history: createWebHistory(),
   routes,
 });
-
-const app = createApp(App);
 app.use(router);
+
 app.mount('#app');
